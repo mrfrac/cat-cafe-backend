3189b8aa6729895eb9cce1118356c900c0c6b51e
mrfrac <fracturer@gmail.com>
2023-02-03 16:23:05 +0500

    ADD TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '../configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        username: configService.get<string>('dbUsername'),
+        password: configService.get<string>('dbPassword'),
+        database: configService.get<string>('dbName'),
+        host: configService.get<string>('dbHost'),
+        port: configService.get<number>('dbPort'),
+      }),
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
     }),
   ],
   controllers: [AppController],
